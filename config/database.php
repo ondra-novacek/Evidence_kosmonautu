e4f603605a72359808ec6c25d9c0985f806384ba
Ondra Novacek <xnovace3@mendelu.cz>
2019-01-17 16:19:52 +0100

    Database setup on Heroku - corrected spelling mistake

--- config/database.php
+++ config/database.php
@@ -44,7 +44,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'kosmonatidb'),
+            'database' => env('DB_DATABASE', 'kosmonautidb'),
             'username' => env('DB_USERNAME', 'kosmonautadmin'),
             'password' => env('DB_PASSWORD', 'mysecretpassword1'),
             'unix_socket' => env('DB_SOCKET', ''),
